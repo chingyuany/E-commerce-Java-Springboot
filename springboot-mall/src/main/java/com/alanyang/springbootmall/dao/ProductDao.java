a5d3a9c6dfe0077c3825c145f8989f76390498c1
chingyuany <chingyy@g.clemson.edu>
2022-12-29 16:41:49 -0500

    Product function: implement query all products fun

--- springboot-mall/src/main/java/com/alanyang/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/alanyang/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.alanyang.springbootmall.dao;
 import com.alanyang.springbootmall.dto.ProductRequest;
 import com.alanyang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
 
--- springboot-mall/src/main/java/com/alanyang/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/alanyang/springbootmall/service/ProductService.java
@@ -3,11 +3,16 @@ package com.alanyang.springbootmall.service;
 import com.alanyang.springbootmall.dto.ProductRequest;
 import com.alanyang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
+
 }
